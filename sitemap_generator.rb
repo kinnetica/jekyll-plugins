7be3d4a25eeb0aed8674d7ca9cb91935d31d0a90
allard <johan@allardsoft.com>
2016-02-10 14:43:35 +1100

    Added the ability to use when setting source directory in jekyll config.

--- sitemap_generator.rb
+++ sitemap_generator.rb
@@ -126,7 +126,7 @@ module Jekyll
     def fill_pages(site, urlset)
       site.pages.each do |page|
         if !excluded?(site, page.path_to_source)
-          if File.exists?(page.path)
+          if File.exists?(File.join(site.source, page.path))
             url = fill_url(site, page)
             urlset.add_element(url)
           end
@@ -192,7 +192,7 @@ module Jekyll
     # Returns lastmod REXML::Element or nil
     def fill_last_modified(site, page_or_post)
       lastmod = REXML::Element.new "lastmod"
-      date = File.mtime(page_or_post.path)
+      date = File.mtime(File.join(site.source, page_or_post.path))
       latest_date = find_latest_date(date, site, page_or_post)
 
       if @last_modified_post_date == nil
